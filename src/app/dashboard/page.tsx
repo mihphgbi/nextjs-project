8c9849a49f0f3ab9b3efd8b4b04bfe97dd9436bd
anna <anna.bui@pal.net.vn>
2024-03-22 17:38:11 +0700

    change UI of page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,7 +3,11 @@ import Link from "next/link";
 export default function DashboardPage() {
     return (
         <>
-            <p>Dashboard</p>
+            <main>
+                <h1 className={`mb-4 text-xl md:text2xl`}>Dashboard</h1>
+                <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4"></div>
+                <div className={"mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8"}></div>
+            </main>
         </>
     )
 }
